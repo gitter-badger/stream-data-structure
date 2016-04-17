eb214c2b14765145944fd44951ff37f8eb4dcf34
Damian Warszawski <dws@touk.pl>
2016-04-17 12:55:48 +0200

    additional task

--- stream-workshop/src/main/java/stream/exercises/ex1/operations/RegionDao.java
+++ stream-workshop/src/main/java/stream/exercises/ex1/operations/RegionDao.java
@@ -1,15 +1,12 @@
 package stream.exercises.ex1.operations;
 
-import stream.exercises.ex1.operations.Region.Wojewodztwo;
-
 import java.util.List;
 import java.util.Map;
 
 public class RegionDao {
     public List<Region> removeMazowieckie() {
         List<Region> list = Region.RegionRepository.bezrobocie2014();
-        list.removeIf(region -> region.getWojewodztwo().equals(Wojewodztwo.MAZOWIECKIE));
-        return list;
+        throw new UnsupportedOperationException("Missing implementation");
     }
 
     public List<Region> findRegionsWithUnemploymentHigherThan10Percent() {
